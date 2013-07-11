51520bbb86c0296115ba7679ff72bfb5ee6062e6
Tim Robertson <trobertson@netspend.com>
2013-07-10 20:44:48 -0500

    make stuff work

---
+++ README.md
@@ -0,0 +1,35 @@
+# Node Test Flight
+
+A simple node module for pushing builds IPA and APK files to TestFlight.
+
+
+	npm install -g testflight
+
+
+Usage
+
+# Edit testflight.json to include your keys and paths
+
+	testflight # This will use testfight.json
+
+# Pass in command line parameters
+
+	  Usage: testflight [options]
+
+	  Options:
+
+	    -h, --help                        output usage information
+	    -V, --version                     output the version number
+	    -b  --binary [path]               The path to the IPA or APK binary
+	    -a  --api-token [token]           The TestFlight api token
+	    -t  --team-token [token]          The TestFlight team token
+	    -n  --notes [notes]               Optional notes
+	    -d  --dsym [path]                 For IPAs, the location of the dysm file
+	    -l  --distribution-lists [lists]  The distribution lists to be notified
+	    -n  --notify                      Should notifications be sent to qualified TestFlight users
+	    -r  --replace                     Should this upload replace existing builds
+	    -v  --verbose                     
+	    --debug                           
+		
+
+
---
+++ index.js
@@ -0,0 +1,123 @@
+var FormData = require('form-data');
+var fs = require('fs');
+var tfConfig;
+var commander = require('commander');
+var verbose = false;
+var debug = false;
+
+if(fs.existsSync('./testflight.json')){
+	console.log('Using configuration in testflight.json file');
+	tfConfig = require('./testflight');
+} else {
+	console.log('testflight.json does not exist');
+	tfConfig = {};
+}
+
+commander.
+	version('0.0.1').
+	option('-b  --binary [path]', 'The path to the IPA or APK binary').
+	option('-a  --api-token [token]', 'The TestFlight api token').
+	option('-t  --team-token [token]', 'The TestFlight team token').
+	option('-n  --notes [notes]', 'Optional notes').
+	option('-d  --dsym [path]', 'For IPAs, the location of the dysm file').
+	option('-l  --distribution-lists [lists]', 'The distribution lists to be notified').
+	option('-n  --notify', 'Should notifications be sent to qualified TestFlight users').
+	option('-r  --replace', 'Should this upload replace existing builds' ).
+	option('-v  --verbose').
+	option('--debug').
+	parse(process.argv);
+
+// console.log(commander);
+
+function processArgs() {
+
+	if(commander.verbose) {
+		verbose = true;
+	}
+
+	if(commander.debug){
+		debug = true;
+	}
+
+	if(commander.binary) {
+		tfConfig.binary = commander.binary;
+	}
+
+	if(commander['api-token']) {
+		tfConfig.postData.api_token = commander['api-token'];
+	}
+
+	if(commander['team-token']) {
+		tfConfig.postData.team_token = commander['team-token'];
+	}
+
+	if(commander.notes) {
+		tfConfig.postData.notes = commander.notes;
+	}
+
+	if(commander.dsym) {
+		tfConfig.postData.dsym = commander.dsym;
+	}
+
+	if(commander['distribution-lists']) {
+		tfConfig.postData.distribution_lists = commander['distribution-lists'];
+	}
+
+	if(commander.notify) {
+		tfConfig.postData.notify = commander.notify;
+	}
+
+	if(commander.replace) {
+		tfConfig.postData.replace = commander.replace;
+	}
+
+	if(verbose) {
+		console.log('using configuration', tfConfig);
+		console.log('about to post to ', tfConfig.url);
+	}
+}
+
+function assertRequiredParams() {
+	if(typeof tfConfig.postData.api_token !== 'string' || tfConfig.postData.api_token.length === 0){
+		console.error('Missing required parameter api_token');
+		process.exit(1);
+	}
+	if(typeof tfConfig.postData.team_token !== 'string' || tfConfig.postData.team_token.length === 0){
+		console.error('Missing required parameter team_token');
+		process.exit(1);
+	}
+	if(typeof tfConfig.binary !== 'string' || tfConfig.binary.length === 0){
+		console.error('Missing required parameter binary');
+		process.exit(1);
+	}
+}
+
+processArgs();
+assertRequiredParams();
+
+
+
+process.exit(1);
+
+var form = new FormData();
+form.append('api_token', tfConfig.postData.api_token);
+form.append('team_token', tfConfig.postData.team_token);
+form.append('file', fs.createReadStream(tfConfig.binary));
+form.append('notes', tfConfig.postData.notes);
+
+if(debug){
+	console.log('Form data is...');
+	console.log(form);
+}
+
+
+form.submit('http://testflightapp.com/api/builds.json', function(err, res){
+	if(err){
+		console.log('********** error ***********');
+		console.log(err);
+		console.log(res);
+	}else {
+		console.log('********************************');
+		console.log(res.statusCode);
+	}
+});
